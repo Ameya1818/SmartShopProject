1e0843738aaecbd306a4264f49b5f3e1fba112e6
Ameya1818 <ameyashingane0109@gmail.com>
2025-09-12 12:11:19 +0530

    productname added

--- SmartShop/src/com/smartshop/model/Order.java
+++ SmartShop/src/com/smartshop/model/Order.java
@@ -3,6 +3,8 @@
 public class Order {
 	private String product_ID="";
 	private String order_ID="";
+	private String user_ID="";
+	private String productName;
 	public static void main(String[] args) {
 		
 	
